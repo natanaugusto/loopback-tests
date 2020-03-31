a2c2a57e39017c0dbe64dae83ac6850a15c49178
Natan Augusto <natanaugusto@gmail.com>
2020-03-31 14:15:39 -0300

    Create ProductController

--- src/repositories/product.repository.ts
+++ src/repositories/product.repository.ts
@@ -1,13 +1,13 @@
-import {DefaultCrudRepository} from '@loopback/repository';
-import {Product, ProductRelations} from '../models';
-import {LocaldbDataSource} from '../datasources';
 import {inject} from '@loopback/core';
+import {DefaultCrudRepository} from '@loopback/repository';
+import {LocaldbDataSource} from '../datasources';
+import {Product, ProductRelations} from '../models';
 
 export class ProductRepository extends DefaultCrudRepository<
   Product,
   typeof Product.prototype.id,
   ProductRelations
-> {
+  > {
   constructor(
     @inject('datasources.localdb') dataSource: LocaldbDataSource,
   ) {
